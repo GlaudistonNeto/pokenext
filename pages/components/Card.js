9841eec7c9f58d08221f2f7abe751e2ab7a0d3a9
GlaudistonNeto <glaudistonson@gmail.com>
2022-01-19 20:45:20 -0300

    finalizing the home page

---
+++ pages/components/Card.js
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import styles from '../../styles/Card.module.css'
+
+export default function Card({ pokemon }) {
+  return (
+    <div className={styles.card}>
+    <Image
+      src={`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`}
+      height="120"
+      width="120"
+      alt={pokemon.name}
+    />
+      <p className={styles.id}>{pokemon.id}</p>
+      <h3>{pokemon.title}</h3>
+      <Link href={`/pokemon/${pokemon.id}`}>
+        <a className={styles.btn}>Details</a>
+      </Link>
+    </div>
+  );
+}
